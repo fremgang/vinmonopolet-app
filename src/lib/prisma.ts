a0f3078f3cef922eb6dbeb6506bb7697db8c38d1
fremgang <jonas.thoresen@gmail.com>
2025-03-09 09:07:51 +0000

    optimize

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,8 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { withOptimize } from "@prisma/extension-optimize";
 
 const prisma = new PrismaClient().$extends(withOptimize()).$extends(withAccelerate())
 
 export { prisma };
-
-function withOptimize() {
-    const extension = (client: any) => client;
-    extension.$extends = { extArgs: {} };
-    return extension;
-}
