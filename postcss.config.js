0139ec2a1f65ea1fb29cc51788f7d5e1aa0a0bcc
fremgang <jonas.thoresen@gmail.com>
2025-03-09 00:20:04 +0000

    yy

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 // postcss.config.js
 export default {
   plugins: {
-    "@tailwindcss/postcss": {
-      content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
