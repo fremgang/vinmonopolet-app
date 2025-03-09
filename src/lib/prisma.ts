4fb8c6d25561f0e325a5a6cdeb932e2bb5a6bc72
fremgang <jonas.thoresen@gmail.com>
2025-03-09 11:00:13 +0000

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,12 @@
-import { Prisma } from '@prisma/client/edge';
-import { PrismaClient } from '@prisma/client';
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+
+export function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
+// Singleton to avoid too many client instances in development
+const globalForPrisma = global as unknown as { prisma: ReturnType<typeof createPrismaClient> };
+export const prisma = globalForPrisma.prisma || createPrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
