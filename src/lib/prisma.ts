b4fc888da8db4257323fc7d4075e3da193fb607c
fremgang <jonas.thoresen@gmail.com>
2025-03-09 03:57:04 +0000

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,8 +4,4 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient().$extends(withAccelerate())
 
-
-// Store in global object to prevent multiple instances in development
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
 export { prisma };
