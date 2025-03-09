66b9aa117b556501069be085aaacb3510371423c
fremgang <jonas.thoresen@gmail.com>
2025-03-09 12:18:40 +0000

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,9 @@
-///** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'tailwindcss/nesting': {},
-  },
-};
-
-export default config;
 
 // postcss.config.js
-// export default {
+export default {
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
-// };
+ };
