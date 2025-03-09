b9a28a01fd07d907c89834ed4461689744a0bf9d
fremgang <jonas.thoresen@gmail.com>
2025-03-09 09:58:27 +0000

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client/edge';
-import { withOptimize } from '@prisma/extension-optimize';
+// import { withOptimize } from '@prisma/extension-optimize';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient().$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })).$extends(withAccelerate())
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export { prisma };
